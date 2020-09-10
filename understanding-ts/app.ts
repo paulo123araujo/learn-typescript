c6b53bbf8d30a7cb1cd7573df4e236250c015d7d
Paulo Araújo <paulofelipe_jau7654@hotmail.com>
2020-09-10 19:19:21 -0300

    learn how to deal with some situations

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -12,9 +12,44 @@ function add(
     return result;
 }
 
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+}
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: number
+} = {
+    name: 'Paulo',
+    age: 22,
+    hobbies: ['Soccer', 'Fishing'],
+    role: Role.ADMIN
+};
+
+// person.role.push('admin'); // this is an exception of ts
+// person.role[1] = 10; // this not follow the rule
+
+// person.role = [0, 'admin', 'user']; // this not follow the rule
+
+console.log(person);
+
 const number1 = 3;
 const number2 = 52;
 const showResult = true;
 const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2, showResult, resultPhrase);
+add(number1, number2, showResult, resultPhrase);
+
+function combine(input1: number|string, input2: number|string)
+{
+    if(typeof input1 === 'number' && typeof input2 === 'number') {
+        return input1 + input2;
+    }
+
+    return input1.toString() + input2.toString();
+}
+
